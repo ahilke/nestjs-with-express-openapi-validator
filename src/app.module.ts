a39f9f3c2f51359889b2551e0be4db1d84b5b8a1
Arno Hilke <arno.hilke@tum.de>
2021-02-11 18:13:09 +0100

    Add Express OpenAPI Validator Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
-import { Module } from "@nestjs/common";
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { PingModule } from "./modules/ping/ping.module";
 import { ConfigModule } from "@nestjs/config";
+import * as OpenApiValidator from "express-openapi-validator";
+import { join } from "path";
 
 @Module({
     imports: [ConfigModule.forRoot(), PingModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        const middlewares = OpenApiValidator.middleware({
+            apiSpec: join(__dirname, "./openapi.yaml"),
+            validateRequests: {
+                allowUnknownQueryParameters: true,
+                coerceTypes: false,
+            },
+            validateResponses: true,
+            validateFormats: "full",
+        });
+
+        consumer.apply(...middlewares).forRoutes("*");
+    }
+}
